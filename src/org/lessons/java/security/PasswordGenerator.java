df0bc2052b07c21bae5dd06de452e07c45ea2a3c
Pietromuresu <muresupietro66@gmail.com>
2023-09-13 15:55:28 +0200

    Generate password from user's data

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,5 +1,36 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Write your name here: ");
+		String name = sc.nextLine();
+		
+		System.out.println("Write your last name here: ");
+		String lastName = sc.nextLine();
+		
+		
+		System.out.println("Write your favourite color here: ");
+		String favouriteColor = sc.nextLine();
+		
+		System.out.println("Write the day you were born here: ");
+		int birthDay = sc.nextInt();
+		
+		System.out.println("Write the month(in numbers) you were born here: ");
+		int birthMonth = sc.nextInt();
+		
+		System.out.println("Write the year you were born here: ");
+		int birthYear = sc.nextInt();
+		
+		int sumDate	= 	birthDay + birthMonth + birthYear;
+		String newPassword = name + '-' + lastName + '-' + favouriteColor + '-' + sumDate;
+		
+		System.out.println(newPassword);
+		
+		sc.close();
+	}
 }
